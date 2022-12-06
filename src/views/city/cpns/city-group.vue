83093b92863f46c583e4fe37302e10a9dfaa0ca1
wenhuikaa <wenhui4001@gmail.com>
2022-12-06 20:00:57 +0800

    城市列表的索引动态映射

--- src/views/city/cpns/city-group.vue
+++ src/views/city/cpns/city-group.vue
@@ -1,31 +1,60 @@
 <template>
     <div class="city-group">
-        <van-index-bar>
-            <template v-for="(group,index) in groupData.cities" :key="index">
+        <van-index-bar :index-list="indexList" highlight-color="#ff9587">
+            <van-index-anchor index="热门" />
+            <div class="list">
+                <template v-for="(city, index) in groupData.hotCities">
+                    <div class="city">
+                        {{ city.cityName }}
+                    </div>
+                </template>
+            </div>
+            <template v-for="(group, index) in groupData.cities" :key="index">
                 <van-index-anchor :index="group.group" />
-                <div class="list">
-                    <template v-for="(city,indey) in group.cities" :key="indey">
-                        <div class="city">
-                            <van-cell :title="city.cityName" />
-                        </div>
-                    </template>
-                </div>
+                <template v-for="(city, indey) in group.cities" :key="indey">
+                    <van-cell :title="city.cityName" />
+                </template>
             </template>
         </van-index-bar>
     </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 
-defineProps({
+const props = defineProps({
     groupData: {
         type: Object,
         default: () => ({})
     }
 })
 
+const indexList = computed(() => {
+    let list = props.groupData.cities.map(_ => _.group)
+    list.unshift('#')
+    return list
+})
+
 </script>
 
 <style lang="less" scoped>
+.list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 10px;
+    padding-right: 25px;
 
+    .city {
+        width: 70px;
+        height: 28px;
+        border-radius: 14px;
+        background-color: #fff4ec;
+        font-size: 12px;
+        color: #000;
+        text-align: center;
+        line-height: 28px;
+        margin: 10px 0;
+    }
+}
 </style>
